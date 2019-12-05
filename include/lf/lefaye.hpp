278c8403c88199fdae3ea204d39e06e29d58962e
Nicholas Carpenetti <nickcarpenetti@gmail.com>
2019-12-05 01:40:24 -0800

    added clangd language server support, empty init/shutdown functions

--- include/lf/lefaye.hpp
+++ include/lf/lefaye.hpp
@@ -1,6 +1,9 @@
 #include <cstdint>
 
 namespace lf {
+  void init(uint32_t width, uint32_t height);
+  void shutdown();
+
   uint32_t getVulkanExtensionCount();
   int add_vals(int a, int b);
 }
--- src/lefaye.cpp
+++ src/lefaye.cpp
@@ -4,6 +4,12 @@
 #include <vulkan/vulkan.h>
 
 namespace lf {
+  void init(uint32_t width, uint32_t height) {
+  }
+
+  void shutdown() {
+  }
+  
   uint32_t getVulkanExtensionCount() {
     uint32_t extensionCount;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
